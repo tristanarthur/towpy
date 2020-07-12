a4ee4a455050d998748be53466a7b8837886a344
Tristan Arthur <tristanarthur2000@gmail.com>
2020-07-12 22:26:31 +1000

    Made into a basic package

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="TOWPY",
-    version="0.1",
+    name="towpy",
+    version="0.1.1",
     url="https://github.com/treestein/TOW.PY",
     author="Tristan Arthur",
     author_email="tristanarthur2000@gmail.com",
     description="Text Only Window",
     packages=find_packages(exclude=["tests"]),
+    long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
     zip_safe=False,
 )
