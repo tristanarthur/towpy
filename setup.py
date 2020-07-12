200d59727531b49f3ceb262336d339ebd95e601b
Tristan Arthur <tristanarthur2000@gmail.com>
2020-07-12 15:30:54 +1000

    Added setup.py file for a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TOWPY",
+    version="0.1",
+    url="https://github.com/treestein/TOW.PY",
+    author="Tristan Arthur",
+    author_email="tristanarthur2000@gmail.com",
+    description="Text Only Window",
+    packages=find_packages(exclude=["tests"]),
+    long_description=open("README.md").read(),
+    zip_safe=False,
+)
