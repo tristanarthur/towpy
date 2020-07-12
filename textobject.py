1d6ab2e0220570b494446e3b025a973a94aa0d0b
Tristan Arthur <tristanarthur2000@gmail.com>
2020-07-12 13:09:34 +1000

    Added rendering of objects

--- textobject.py
+++ textobject.py
@@ -1,16 +1,43 @@
 class TextAnimation:
 
+    ANIMATION_STYLE = {"NORMAL": 1, "REVERSE": -1}
+
     def __init__(self):
-        pass
+        self.duration = 0
+        self.style = None
+
+
 
 class TextObject:
 
-    def __init__(self):
-        self.pos = (0, 0)
-        self.default_text = []
+    def __init__(self, text, pos):
+        self.position = list(pos)
+        self.default_text = self.__load_text(text)
         self.animations = {}
         self.hidden = False
         self.position_gridded = True
+        self.event_handlers = []
 
-    def load(self, file):
+    def update(self, dt):
+        # This method should be overrided by child object
         pass
+
+    def handle_events(self, events):
+        # This method should be overrided by child object
+        pass
+
+    def __load_text(self, text):
+        formatted_text = []
+        if type(text) is str:
+            formatted_text = text.split("\n")
+        elif type(text) is list:
+            formatted_text = text
+        return formatted_text
+
+    @staticmethod
+    def load_from_file(self, file, pos):
+        with open(file) as f:
+            return TextObject(f.read(), pos)
+
+    def get_current_frame(self):
+        return self.default_text
--- window.py
+++ window.py
@@ -1,5 +1,12 @@
 import pygame
 from sys import exit
+from textobject import TextObject
+
+
+# TODO: Have font rendering being stored in text object instead of doing
+#       it on every render frame <---- DO THIS
+#       Will be useful as well because testing for collisions including mouse
+#       collisions will be difficult with the current split
 
 
 class TextOnlyWindow:
@@ -7,6 +14,8 @@ class TextOnlyWindow:
     def __init__(self):
         if not pygame.get_init():
             pygame.init()
+        if not pygame.font.get_init():
+            pygame.font.init()
         self.running = True
 
         self.surface = pygame.display.set_mode((500, 500))
@@ -17,23 +26,91 @@ class TextOnlyWindow:
         self.target_FPS = 15
         self.dt = 0
 
-    def update(self):
+        self.font = pygame.font.SysFont("monospace", 15)
+
+        self.background_colour = (0, 0, 0)
+        self.text_objects = []
+
+    def __update(self):
         self.dt = self.clock.tick(self.target_FPS)
         self.run_time += self.dt
 
-    def render(self):
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.running = False
+
+        for text_object in self.text_objects:
+            text_object.update(self.dt)
+            text_object.handle_events(events)
+
+    def __render(self):
+        self.surface.fill(self.background_colour)
+
+        for text_object in self.text_objects:
+            if not text_object.hidden:
+                self.__render_object(text_object)
+
         pygame.display.update()
 
     def run(self):
         while self.running:
-            self.update()
-            self.render()
+            self.__update()
+            self.__render()
         self.quit()
 
     def quit(self):
+        print("Goodbye!")
         if pygame.get_init():
             pygame.quit()
 
+    def add_object(self, text_object):
+        self.text_objects.append(text_object)
+
+    def __render_object(self, text_object):
+        x, y = text_object.position
+
+        # Snap to grid
+        if text_object.position_gridded:
+            x = x - (x % self.font.size(" ")[0])
+            y = y - (y % self.font.size(" ")[1])
+
+        # Must be stored for line restore point
+        initial_x = x
+
+        for line in text_object.get_current_frame():
+            for char in line:
+                if char is not None:
+                    self.surface.blit(self.font.render(char, False, (255, 255, 255)), (x, y))
+                x += self.font.size(" ")[0]
+            # Reset x to start of object and move
+            # y to next line
+            x = initial_x
+            y += self.font.size(" ")[1]
+
+
+    def set_background_colour(self, colour):
+        if type(colour) is tuple and len(colour) == 3:
+            self.background_colour = colour
+        else:
+            raise TypeError("Incorrect background colour format!")
+
 
 if __name__ == "__main__":
-    TextOnlyWindow().run()
+    tow = TextOnlyWindow()
+    tow.set_background_colour((100, 100, 100))
+
+    a = TextObject([[" __ "],
+                    ["|__|"]],
+                    (50, 50))
+    tow.add_object(a)
+
+    b = TextObject("Hello\nMy\nName\nTree", (200, 200))
+    tow.add_object(b)
+
+    c = TextObject([["I", None, "I"],
+                    ["I", "_", "I"]],
+                    (100, 100))
+    tow.add_object(c)
+
+    tow.run()
